cec641c8bb574c60136c58fa32c190c35907482e
SAEG64 <95483294+SAEG64@users.noreply.github.com>
2024-03-05 13:48:28 +0100

    Update and rename fora_mixed_effects_beta_plots_meanLines.py to fora_mixed_effects_beta_plots_rthreat.py

--- fora_mixed_effects_beta_plots_rthreat.py
+++ fora_mixed_effects_beta_plots_rthreat.py
@@ -60,22 +60,22 @@ for itr, fle in enumerate(glob.glob(path + "DATA_clean/DATA_fitted/test_data.*.C
 
 # Prepare data for plotting
 df11 = pd.DataFrame(coefs_cond1[0])
-df11['Coefficients'] = r"$\beta_0$"
+df11['Coefficients'] = r"$\beta_0 (intercept)$"
 df12 = pd.DataFrame(coefs_cond1[1])
-df12['Coefficients'] = r"$\beta_1$"
+df12['Coefficients'] = r"$\beta_1 (slope)$"
 df3 = pd.concat([df11, df12], axis = 0)
 df3['Conditions'] = "loose"
 df21 = pd.DataFrame(coefs_cond2[0])
-df21['Coefficients'] = r"$\beta_0$"
+df21['Coefficients'] = r"$\beta_0 (intercept)$"
 df22 = pd.DataFrame(coefs_cond2[1])
-df22['Coefficients'] = r"$\beta_1$"
+df22['Coefficients'] = r"$\beta_1 (slope)$"
 df4 = pd.concat([df21, df22], axis = 0)
 df4['Conditions'] = "tense"
 df_stack = pd.concat([df3, df4], axis = 0).reset_index(drop=True)
 df_stack = df_stack.rename(columns = {0:'β distributions'})
-df_beta0 = df_stack[df_stack['Coefficients'] == r"$\beta_0$"]
+df_beta0 = df_stack[df_stack['Coefficients'] == r"$\beta_0 (intercept)$"]
 df_beta0 = df_beta0.reset_index(drop=True)
-df_beta1 = df_stack[df_stack['Coefficients'] == r"$\beta_1$"]
+df_beta1 = df_stack[df_stack['Coefficients'] == r"$\beta_1 (slope)$"]
 df_beta1 = df_beta1.reset_index(drop=True)
 
 ## Plotting
@@ -107,50 +107,4 @@ plt.xlabel("Competition level", fontsize=26)
 # Customize legend
 handles, labels = ax.get_legend_handles_labels()
 l = plt.legend(handles[0:2], labels[0:2], bbox_to_anchor=(1.05, 0.5), loc=2, 
-               borderaxespad=0., fontsize=20, title='Coefficients', 
-               title_fontsize=20)
-
-# # Stripplot beta0 with mean line
-# fig, ax = plt.subplots(figsize=(6, 6), dpi = 600)
-# sns.stripplot(x="Conditions", y="β distributions",
-#             hue="Coefficients", palette=["m","g"],
-#             data=df_stack, dodge=True, ax=ax)
-# sns.lineplot(x="Conditions", y="β distributions",
-#             hue="Coefficients", palette=["m","g"],
-#             data=df_stack, ci = None, alpha = 0.5, ax=ax)
-# ax.set_title('Interaction threat risk',
-#               loc ='left', size = 29)
-# # Customize ticks
-# ax.tick_params(bottom=True, left=True, size=5, direction= "in")
-# ax.tick_params(axis="x", labelsize=20)
-# ax.tick_params(axis="y", labelsize=20, labelbottom = False)
-# plt.ylabel("β distributions", fontsize=26)
-# plt.xlabel("Competition level", fontsize=26)
-# # Customize legend
-# handles, labels = ax.get_legend_handles_labels()
-# l = plt.legend(handles[0:2], labels[0:2], bbox_to_anchor=(1.05, 0.5), loc=2, 
-#                 borderaxespad=0., fontsize=20, title='Coefficients', 
-#                 title_fontsize=20)
-# plt.tick_params(axis='x', which='minor')
-
-# # Stripplot beta1 with mean line
-# fig, ax = plt.subplots(figsize=(6, 6), dpi = 600)
-# sns.stripplot(x="Conditions", y="β distributions",
-#             hue="Coefficients", palette=["g"],
-#             data=df_beta1, dodge=True)
-# sns.lineplot(x="Conditions", y="β distributions",
-#             hue="Coefficients", palette=["g"],
-#             data=df_beta1, ci = None, alpha = 0.5)
-# ax.set_title('Interaction threat risk',
-#               loc ='left', size = 29)
-# # Customize ticks
-# ax.tick_params(bottom=True, left=True, size=5, direction= "in")
-# ax.tick_params(axis="x", labelsize=20)
-# ax.tick_params(axis="y", labelsize=20, labelbottom = False)
-# plt.ylabel("β distributions", fontsize=26)
-# plt.xlabel("Competition level", fontsize=26)
-# # Customize legend
-# handles, labels = ax.get_legend_handles_labels()
-# l = plt.legend(handles[0:2], labels[0:2], bbox_to_anchor=(1.05, 0.5), loc=2, 
-#                 borderaxespad=0., fontsize=20, title='Coefficients', 
-#                 title_fontsize=20)
+               borderaxespad=0., fontsize=20)
