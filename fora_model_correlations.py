a47fba11e869f427dd0f5395609337a9608006f5
SAEG64 <95483294+SAEG64@users.noreply.github.com>
2024-12-06 14:57:41 +0100

    Update fora_model_correlations.py

--- fora_model_correlations.py
+++ fora_model_correlations.py
@@ -5,63 +5,6 @@ Created on Mon Jun 26 11:23:18 2023
 
 @author: sergej
 """
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# # import matplotlib as mpl
-# import pandas as pd
-# import os
-# path = os.path.dirname(__file__)+"/"
-
-# # List of model names
-# mdlName =   ['win stay lose shift',
-#             '** wait when safe',
-#             '** binary energy state',
-#             'weather type',
-#             '* $\\mathit{r}$ predator',
-#             '* $\\mathit{p}$ gain',
-#             'expected gain naive',
-#             '** $\\mathit{p}$ success',
-#             'marginal value',
-#             'multi-heuristic policy',
-#             '$\mathit{OP}$ values + cap',
-#             'optimal policy values']
-
-# data = pd.read_csv(path + "DATA_clean/DATA_group_level/test_data.group_level.csv")[mdlName]
-# data['$\mathit{p}$ success'] = data['** $\mathit{p}$ success']
-# del data['** $\mathit{p}$ success']
-# data['$\mathit{p}$ gain'] = data['* $\mathit{p}$ gain']
-# del data['* $\mathit{p}$ gain']
-# data['$\mathit{r}$ predator'] = data['* $\\mathit{r}$ predator']
-# del data['* $\\mathit{r}$ predator']
-# data['binary energy state'] = data['** binary energy state']
-# del data['** binary energy state']
-# data['wait when safe'] = data['** wait when safe']
-# del data['** wait when safe']
-
-# mdlName =   ['win stay lose shift',
-#             'wait when safe',
-#             'binary energy state',
-#             'weather type',
-#             '$\\mathit{r}$ predator',
-#             '$\\mathit{p}$ gain',
-#             'expected gain naive',
-#             '$\\mathit{p}$ success',
-#             'marginal value',
-#             'multi-heuristic policy',
-#              '$\mathit{OP}$ values + cap',
-#             'optimal policy values']
-# data = data.reindex(columns=mdlName[::-1])
-
-# # Run and plot correlations
-# matrix = data.corr()
-# fig, ax = plt.subplots(figsize=(10, 8), dpi=600)
-# cmap = sns.cm.rocket_r
-# sns.heatmap(matrix, annot=True, yticklabels=True, 
-#                   xticklabels=True, annot_kws={'size': 12}, cmap = cmap)
-# ax.tick_params(axis="x", labelsize=24, labelrotation=90)
-# ax.tick_params(axis="y", labelsize=24, labelrotation=0)
-# cax = ax.figure.axes[-1]
-# cax.tick_params(labelsize=24)
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -69,6 +12,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 path = os.path.dirname(__file__)+"/"
+
+fltr = False
 
 # List of model names
 mdlName =   ['win stay lose shift',
@@ -83,7 +28,7 @@ mdlName =   ['win stay lose shift',
             'multi-heuristic policy',
             '$\mathit{OP}$ values + cap',
             'optimal policy values']
-
+# Renaming data
 data = pd.read_csv(path + "DATA_clean/DATA_group_level/test_data.group_level_datall.csv")[mdlName]
 data['$\mathit{p}$ success'] = data['** $\mathit{p}$ success']
 del data['** $\mathit{p}$ success']
@@ -95,7 +40,7 @@ data['binary energy state'] = data['** binary energy state']
 del data['** binary energy state']
 data['wait when safe'] = data['** wait when safe']
 del data['** wait when safe']
-
+# Revise list of model names
 mdlName =   ['win stay lose shift',
             'wait when safe',
             'binary energy state',
@@ -108,11 +53,30 @@ mdlName =   ['win stay lose shift',
             'multi-heuristic policy',
              '$\mathit{OP}$ values + cap',
             'optimal policy values']
+
+if fltr == True:
+    # Data fltring
+    data = data[data['multi-heuristic policy'] != 1]
+    data = data[data['multi-heuristic policy'] != 0]
+    # Revise list of model names
+    mdlName =   ['win stay lose shift',
+                # 'wait when safe',
+                # 'binary energy state',
+                'weather type',
+                '$\\mathit{r}$ predator',
+                '$\\mathit{p}$ gain',
+                'expected gain naive',
+                '$\\mathit{p}$ success',
+                'marginal value',
+                'multi-heuristic policy',
+                 '$\mathit{OP}$ values + cap',
+                'optimal policy values']
+
 data = data.reindex(columns=mdlName[::-1])
 
 # Run and plot correlations
 matrix = data.corr()
-fig, ax = plt.subplots(figsize=(10, 8), dpi=600)
+fig, ax = plt.subplots(figsize=(12, 10), dpi=600)
 cmap = sns.cm.rocket_r
 sns.heatmap(matrix, annot=True, yticklabels=True, 
                   xticklabels=True, annot_kws={'size': 12}, cmap = cmap)
